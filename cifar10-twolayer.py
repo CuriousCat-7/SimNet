75eb3cf85d9a1f9d3a82d958e25ef2ad9e00d162
limingyao <limingyao@ainirobot.com>
2018-06-12 00:31:25 +0800

    just for save

--- cifar10-twolayer.py
+++ cifar10-twolayer.py
@@ -8,7 +8,7 @@ import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, Input, InputLayer
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, InputLayer, AveragePooling2D
 from keras.layers import Conv2D, MaxPooling2D
 from keras.backend.tensorflow_backend import set_session
 from keras import backend as K
@@ -17,12 +17,12 @@ import simnets.keras as sk
 import numpy as np
 import os
 import pdb
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 set_session(tf.Session(config=config))
 
-batch_size = 128
+batch_size = 32
 num_classes = 10
 epochs = 100
 data_augmentation = True
@@ -49,7 +49,7 @@ model.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', use_bias=None,\
 model.add(sk.Similarity(32, blocks=[1,1], strides=[1,1], similarity_function='L2',normalization_term=True, padding=[1,1],\
     out_of_bounds_value=np.nan, ignore_nan_input=True ))
 model.add(sk.Mex(32,
-              blocks=[32, 3, 3], strides=[32, 2, 2],
+              blocks=[1, 3, 3], strides=[32, 2, 2],
               softmax_mode=False, normalize_offsets=True,
               use_unshared_regions=True, unshared_offset_region=[2]))
 model.add(Conv2D(filters=64, kernel_size=(5,5), padding='same', use_bias=None,\
@@ -57,13 +57,14 @@ model.add(Conv2D(filters=64, kernel_size=(5,5), padding='same', use_bias=None,\
 model.add(sk.Similarity(64, blocks=[1,1], strides=[1,1], similarity_function='L2',normalization_term=True, padding=[1,1],\
     out_of_bounds_value=np.nan, ignore_nan_input=True ))
 model.add(sk.Mex(64,
-              blocks=[64, 3, 3], strides=[64, 2, 2],
+              blocks=[1, 3, 3], strides=[64, 2, 2],
               softmax_mode=False, normalize_offsets=True,
               use_unshared_regions=True, unshared_offset_region=[2])) #w/4 = 8
 model.add(sk.Mex(10,
-              blocks=[64, 8, 8], strides=[64, 8, 8],
+              blocks=[1, 8, 8], strides=[64, 8, 8],
               softmax_mode=True, normalize_offsets=True,
               use_unshared_regions=True, unshared_offset_region=[2])) #max pooling
+#model.add(keras.layers.AveragePooling2D(pool_size=(8, 8), strides=None, padding='valid', data_format='channels_first'))
 model.add(Flatten(data_format='channels_first'))
 model.summary()
 # initiate SGD with nesterov optimizer
@@ -76,8 +77,10 @@ model.compile(loss='categorical_crossentropy',
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train /= 255 - 0.5
-x_test /= 255 - 0.5
+x_train /= 255
+x_test /= 255
+x_train -= 0.5
+x_test -= 0.5
 
 if not data_augmentation:
     print('Not using data augmentation.')
--- cifar10.py
+++ cifar10.py
@@ -44,15 +44,18 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(InputLayer(input_shape=(3,32,32)))
-model.add(Conv2D(filters=75, kernel_size=(5,5), padding='same', use_bias=None,\
+model.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', use_bias=None,\
     data_format='channels_first', strides=(1,1)))
-model.add(sk.Similarity(75, blocks=[1,1], strides=[1,1], similarity_function='L2',normalization_term=True, padding=[0,0],\
+model.add(sk.Similarity(32, blocks=[1,1], strides=[1,1], similarity_function='L2',normalization_term=True, padding=[1,1],\
     out_of_bounds_value=np.nan, ignore_nan_input=True ))
-model.add(sk.Mex(10,
-              blocks=[75, 16, 16], strides=[75, 16, 16],
+model.add(sk.Mex(32,
+              blocks=[1, 3, 3], strides=[32, 2, 2],
               softmax_mode=False, normalize_offsets=True,
               use_unshared_regions=True, unshared_offset_region=[2]))
-model.add(keras.layers.AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format='channels_first'))
+model.add(sk.Mex(10,
+              blocks=[1, 16, 16], strides=[32, 16, 16],
+              softmax_mode=True, normalize_offsets=True,
+              use_unshared_regions=True, unshared_offset_region=[2]))
 model.add(Flatten(data_format='channels_first'))
 model.summary()
 # initiate SGD with nesterov optimizer
@@ -65,8 +68,10 @@ model.compile(loss='categorical_crossentropy',
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train /= 255 - 0.5
-x_test /= 255 - 0.5
+x_train /= 255
+x_test /= 255
+x_train -= 0.5
+x_test -= 0.5
 
 if not data_augmentation:
     print('Not using data augmentation.')
